31620825a51a8154292115d79d0f7a29cbe1f8f1
filin155 <artem.manzar145@gmail.com>
2021-04-09 16:18:54 +0300

    completed task 2

--- sandbox/src/main/java/ru/stqa/pft/sandbox/FirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/FirstProgram.java
@@ -3,7 +3,12 @@ package ru.stqa.pft.sandbox;
 public class FirstProgram {
 
     public static void main(String[] args) {
-        
-        System.out.println("Hello world1");
+
+        Point p1 = new Point(0, 1);
+        Point p2 = new Point(2, -2);
+
+        System.out.println(p1.distance(p2));
+
     }
+
 }
