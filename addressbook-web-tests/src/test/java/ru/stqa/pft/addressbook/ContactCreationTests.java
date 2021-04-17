97b15d50593e158a387ccb009cda3704856c91a1
filin155 <artem.manzar145@gmail.com>
2021-04-17 20:41:48 +0300

    refactor test new contact

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
@@ -8,7 +8,7 @@ import org.testng.annotations.*;
 import org.openqa.selenium.*;
 
 
-public class AddNewContact {
+public class ContactCreationTests {
     private WebDriver driver;
 
 
@@ -16,47 +16,73 @@ public class AddNewContact {
     public void setUp() throws Exception {
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+        driver.get("http://localhost/addressbook/index.php");
+        login( "admin", "secret", By.xpath("//input[@value='Login']"));
+    }
+
+    private void login( String userName, String password, By xpath) {
+        driver.findElement(By.name("user")).click();
+        driver.findElement(By.name("user")).clear();
+        driver.findElement(By.name("user")).sendKeys(userName);
+        driver.findElement(By.name("pass")).click();
+        driver.findElement(By.name("pass")).clear();
+        driver.findElement(By.name("pass")).sendKeys(password);
+        driver.findElement(xpath).click();
     }
 
     @Test
-    public void testAddnew() throws Exception {
-        driver.get("http://localhost/addressbook/index.php");
-        driver.findElement(By.name("user")).click();
-        driver.findElement(By.name("user")).clear();
-        driver.findElement(By.name("user")).sendKeys("admin");
-        driver.findElement(By.name("pass")).click();
-        driver.findElement(By.name("pass")).clear();
-        driver.findElement(By.name("pass")).sendKeys("secret");
-        driver.findElement(By.xpath("//input[@value='Login']")).click();
-        driver.findElement(By.linkText("add new")).click();
+    public void testContactCreation() throws Exception {
+        gotoNewContactPage();
+        fillContact(new ContactData("test", "test", "test", "test", "test", "test", "test" ,"test"));
+        submitContactCreation();
+        returnHomePage();
+        logout();
+    }
+
+    private void logout() {
+        driver.findElement(By.linkText("Logout")).click();
+    }
+
+    private void returnHomePage() {
+        driver.findElement(By.linkText("home page")).click();
+    }
+
+    private void submitContactCreation() {
+        driver.findElement(By.name("submit")).click();
+    }
+
+
+
+    private void fillContact(ContactData contactData) {
         driver.findElement(By.name("firstname")).click();
         driver.findElement(By.name("firstname")).clear();
-        driver.findElement(By.name("firstname")).sendKeys("firstName");
+        driver.findElement(By.name("firstname")).sendKeys(contactData.getFirstName());
         driver.findElement(By.name("middlename")).click();
         driver.findElement(By.name("middlename")).clear();
-        driver.findElement(By.name("middlename")).sendKeys("middleName");
+        driver.findElement(By.name("middlename")).sendKeys(contactData.getMiddleName());
         driver.findElement(By.name("lastname")).click();
         driver.findElement(By.name("lastname")).clear();
-        driver.findElement(By.name("lastname")).sendKeys("lastName");
+        driver.findElement(By.name("lastname")).sendKeys(contactData.getLastName());
         driver.findElement(By.name("nickname")).click();
         driver.findElement(By.name("nickname")).clear();
-        driver.findElement(By.name("nickname")).sendKeys("nickname");
+        driver.findElement(By.name("nickname")).sendKeys(contactData.getNickname());
         driver.findElement(By.name("title")).click();
         driver.findElement(By.name("title")).clear();
-        driver.findElement(By.name("title")).sendKeys("title");
+        driver.findElement(By.name("title")).sendKeys(contactData.getTitle());
         driver.findElement(By.name("company")).click();
         driver.findElement(By.name("company")).clear();
-        driver.findElement(By.name("company")).sendKeys("company");
+        driver.findElement(By.name("company")).sendKeys(contactData.getCompany());
         driver.findElement(By.name("address")).click();
         driver.findElement(By.name("address")).clear();
-        driver.findElement(By.name("address")).sendKeys("moscow");
+        driver.findElement(By.name("address")).sendKeys(contactData.getAddress());
         driver.findElement(By.name("home")).click();
         driver.findElement(By.name("home")).clear();
-        driver.findElement(By.name("home")).sendKeys("home");
-        driver.findElement(By.name("theform")).click();
-        driver.findElement(By.name("submit")).click();
-        driver.findElement(By.linkText("home page")).click();
-        driver.findElement(By.linkText("Logout")).click();
+        driver.findElement(By.name("home")).sendKeys(contactData.getHome());
+    }
+
+    private void gotoNewContactPage() {
+        driver.findElement(By.linkText("add new")).click();
     }
 
     @AfterClass(alwaysRun = true)
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactData.java
@@ -0,0 +1,55 @@
+package ru.stqa.pft.addressbook;
+
+public class ContactData {
+    private final String firstName;
+    private final String middleName;
+    private final String lastName;
+    private final String nickname;
+    private final String title;
+    private final String company;
+    private final String address;
+    private final String home;
+
+    public ContactData(String firstName, String middleName, String lastName, String nickname, String title, String company, String address, String home) {
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.nickname = nickname;
+        this.title = title;
+        this.company = company;
+        this.address = address;
+        this.home = home;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getHome() {
+        return home;
+    }
+}
